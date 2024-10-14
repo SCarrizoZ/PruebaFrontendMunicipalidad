d83e3e898f8bcada588145ed469824ab5ba121b0
edv <eduardo.bautista@inacapmail.cl>
2024-10-14 03:54:23 -0300

    pba p-estilo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,29 +3,73 @@ import logo from '../assets/logotipo-muni.png'
 const Sidebar = ({
   isOpened
 }) => {
-  
+
   return (
-    <nav className={`sidebar ${!isOpened ? "closed" : "" }`}>
+    <nav className={`sidebar ${!isOpened ? "closed" : ""}`}>
       <header className="sidebar-header">
         <div className="logo-cont">
           <img className='logo-image' src={logo} alt="Logo de la municipalidad" />
         </div>
         <span>Dashboard Municipal</span>
       </header>
-      <ul className="sidebar-list">
-        <li>
-          <a href="#">Inicio</a>
-        </li>
-        <li>
-          <a href="#">Publicaciones</a>
-        </li>
-        <li>
-          <a href="#">Usuarios</a>
-        </li>
-        <li>
-          <a href="#">Cerrar sesión</a>
-        </li>
-      </ul>
+      <div className="menu">
+        <ul className="menu-list">
+          <li class="nav-link">
+            <a href="#">
+              <i class="bx bx-pie-chart icon"></i>
+              <span class="text nav-text">
+                Dashboard
+              </span>
+            </a>
+          </li>
+          <li class="nav-link">
+            <a href="#">
+              <i class="bx bx-file icon"></i>
+              <span class="text nav-text">
+                Publicaciones
+              </span>
+            </a>
+          </li>
+          <li class="nav-link">
+            <a href="#">
+              <i class="bx bx-file icon"></i>
+              <span class="text nav-text">
+                Anuncios
+              </span>
+            </a>
+          </li>
+          <li class="nav-link">
+            <a href="#">
+              <i class="bx bx-bar-chart-alt-2 icon"></i>
+              <span class="text nav-text">
+                Reportes
+              </span>
+            </a>
+          </li>
+          <li class="nav-link">
+            <a href="#">
+              <i class="bx bx-map-alt icon"></i>
+              <span class="text nav-text">
+                Mapa
+              </span>
+            </a>
+          </li>
+
+
+        </ul>
+
+
+        <div className="bottom-menu">
+          <li class="nav-link">
+            <a href="#">
+              <i class="bx bx-log-out icon"></i>
+              <span class="text nav-text">
+                Cerrar sesión
+              </span>
+            </a>
+          </li>
+        </div>
+      </div>
     </nav>
   )
 }
